08a1cb2d9a62ee9221711867004d295b637a81b9
Rauf GUVEN <mrguven@gmail.com>
2022-11-08 21:59:44 +0100

    update

--- node.js
+++ node.js
@@ -1,5 +1,45 @@
-function myFunction () {
+const { clear } = require("console");
 
-    console.log("hello world")
+/*const name1 = 'mario';
 
-}
+    console.log(name1);
+
+    
+
+const greet = (name2)  => {
+    
+    console.log(`hello, ${name2}`);
+} 
+
+greet('dfg');
+
+
+
+
+// Global Object
+
+const express = require('express');
+const app = express();
+
+
+
+
+let port=8000;
+app.listen(port,()=> console.log(`Sercer on ${port}`));
+
+
+console.log(global);
+
+setTimeout(() => {
+    console.log('in the timeout')
+    clearInterval(int);
+},3000);
+
+const int = setInterval(() => {
+    console.log('in the inerval')
+},1000)
+
+*/
+
+console.log(__dirname);
+console.log(__filename);
