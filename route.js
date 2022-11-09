205f6e28e7d334dffd74c877080bf57f0559f388
Rauf GUVEN <mrguven@gmail.com>
2022-11-09 11:13:01 +0100

    update

--- route.js
+++ route.js
@@ -1,2 +1,11 @@
-const express = require('express');
-const routes = require('./config/')
+const { text } = require('body-parser');
+const { response } = require('express');
+
+
+app
+.route("/home")
+.get((request,response,nexthandler)) => {
+    response.type (""text/html")
+    response.write("<DOCTTYPE html")
+    nexthandler()
+})
