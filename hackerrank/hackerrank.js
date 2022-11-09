37bfad11e2ebd74b9c39cf99fc590602b2679fa9
Rauf GUVEN <mrguven@gmail.com>
2022-11-09 11:13:09 +0100

    update

--- hackerrank/hackerrank.js
+++ hackerrank/hackerrank.js
@@ -1,26 +1,27 @@
 let a = [5, 6, 7];
 let b = [3, 6, 10];
 
-var c =[0,0];
+
 
 
 
 function compareTriplets(a, b) {
-   
+    var c =[0,0];
     for (let i =0;i<a.lenght ; i++){
 if (a[i] > b[i]) {
     
     c[0]=+1;
-    return c;
+    
 }}
 
 for (let i =0;i<b.lenght ; i++){
 if (b[i] > a[i]) {
     
     c[1]=+1;
-    return c;
+    
 }
 
 }
-console.log(c[]);
+return c;
 }
+console.log(compareTriplets(a,b));
