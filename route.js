f98cac3ba816fef27ae00704c540f1334204f19d
Rauf GUVEN <mrguven@gmail.com>
2022-11-10 20:26:16 +0100

    update

--- route.js
+++ route.js
@@ -1,3 +1,4 @@
+/*
 const  express  = require('express');
 const app1 = express()
 
@@ -8,3 +9,53 @@ app1.get('/one',(request,response,nexthandler) => {
 app1.listen(3010, () => console.log('web server running on port 3010'))
 const os = require('os');
 console.log(os);
+
+
+
+*/
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
+
+const fs = require('fs');
+
+//read file
+fs.readFile('blog1.txt', (err,data) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(data,toString());
+});
+
+console.log('list line');
+
+// write file
+
+fs.writeFile('blog1.txt', 'hello world', () => {
+    console.log('file was written');
+})
+
+fs.writeFile('./nodejs/blog2.txt', 'hello world', () => {
+    console.log('file was written again');
+})
+
+
+//directories
+
+
